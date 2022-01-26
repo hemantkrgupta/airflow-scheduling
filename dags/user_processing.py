1d0872a067475fd3523da56cefe44b08142c1cbd
Hemant Kumar <hemant.gt.kumar@gmail.com>
2022-01-26 19:15:26 +0530

    added extracting_user task

--- dags/user_processing.py
+++ dags/user_processing.py
@@ -1,7 +1,9 @@
 from airflow.models import DAG
 from airflow.providers.sqlite.operators.sqlite import SqliteOperator
 from airflow.providers.http.sensors.http import HttpSensor
+from airflow.providers.http.operators.http import SimpleHttpOperator
 from datetime import datetime
+import json
 
 default_args = {
     'start_date': datetime(2020,1,1)
@@ -30,4 +32,13 @@ with DAG('user_processing', schedule_interval = '@daily',
         task_id = 'is_api_available',
         http_conn_id = 'user_api',
         endpoint = 'api/'
+    )
+    
+    extractUser = SimpleHttpOperator(
+        task_id = 'extracting_user',
+        http_conn_id = 'user_api',
+        endpoint = 'api/',
+        method = 'GET',
+        response_filter = lambda response: json.loads(response.text),
+        log_response = True
     )
