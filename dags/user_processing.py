794847d3aec8b11b56aa0ffdfde7e2ce7d04dfbe
Hemant Kumar <hemant.gt.kumar@gmail.com>
2022-01-26 19:42:14 +0530

    added processing_user task

--- dags/user_processing.py
+++ dags/user_processing.py
@@ -2,12 +2,30 @@ from airflow.models import DAG
 from airflow.providers.sqlite.operators.sqlite import SqliteOperator
 from airflow.providers.http.sensors.http import HttpSensor
 from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.operators.python import PythonOperator
 from datetime import datetime
 import json
+from pandas import json_normalize
 
 default_args = {
     'start_date': datetime(2020,1,1)
 }
+
+#ti - task instance
+def _processing_user(ti):
+    users = ti.xcom_pull(task_ids = ["extracting_user"])
+    if not len(users) or results not in users:
+        raise ValueError("User is empty")
+    user = users[0]['results'][0]
+    processed_user = json_normalize({
+        "firstname": user["name"]["first"],
+        "lastname": user["name"]["last"],
+        "country": user["location"]["country"],
+        "username": user["login"]["username"],
+        "password": user["login"]["password"],
+        "email": user["email"]
+    })
+    processed_user.to_csv("/tmp/processed_user.csv", index = None, header = False)
 
 with DAG('user_processing', schedule_interval = '@daily',
     default_args = default_args,
@@ -42,3 +60,10 @@ with DAG('user_processing', schedule_interval = '@daily',
         response_filter = lambda response: json.loads(response.text),
         log_response = True
     )
+    
+    processingUser = PythonOperator(
+        task_id = "processing_user",
+        python_callable = _processing_user
+    )
+    
+    
