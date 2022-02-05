5cd56b240e8354a5a5fa51dbb34301970f5a268d
Hemant Kumar <hemant.gt.kumar@gmail.com>
2022-02-05 16:59:03 +0530

    added parallel dag

---
+++ dags/parallel_dag.py
@@ -0,0 +1,32 @@
+from airflow.providers.bash import BashOperator
+from airflow.models import DAG
+from datetime import datetime
+
+with DAG(
+    dag_id = "parallel_dag",
+    schedule_interval = "@daily",
+    default_args = {"start_date" =datetime("2022-01-01")},
+    catchup = False
+    ) as dag:
+    
+    op1 = BashOperator(
+        task_id = "task_1",
+        bash_command = "sleep 3"
+    )
+    
+    op2 = BashOperator(
+        task_id = "task_2",
+        bash_command = "sleep 3"
+    )
+    
+    op3 = BashOperator(
+        task_id = "task_3",
+        bash_command = "sleep 3"
+    )
+    
+    op4 = BashOperator(
+        task_id = "task_4",
+        bash_command = "sleep 3"
+    )
+    
+    op1 >> [op2, op3] >> op4
